cc717b770586095fdbfd146afe523d7444fe235b
Gautam Yadav <gautamyadav08112005@gmail.com>
2022-10-22 20:11:13 +0530

    Working on assignment upload page

--- LMS/dist/script.js
+++ LMS/dist/script.js
@@ -5,8 +5,6 @@ function readURL(input) {
 
     reader.onload = function(e) {
       $('.image-upload-wrap').hide();
-
-      $('.file-upload-image').attr('src', e.target.result);
       $('.file-upload-content').show();
 
       $('.image-title').html(input.files[0].name);
